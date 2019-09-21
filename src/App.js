cc50bec4020b7c9e555ef0ed5f1b10d35e735b8a
Newton Huynh <nth8@rice.edu>
2019-09-21 14:45:07 -0500

    meow meow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import logo from '/Users/newtonhuynh/Desktop/Coding/my-app/src/NewtonHuynhWillRice.JPG';
 import './App.css';
 
 function App() {
+
+  const[count,setcount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
+        <p> <font size="20" font face="Impact">Newton Huynh</font> </p>
+          <p> <font size="5" color="red" font face="cursive"> Will Rice College</font> </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          woahhh 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/newton-huynh-53623b178/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here for LinkedIn Profile 
         </a>
+        <p> You have clicked me {count} times</p>
+
+        <button className="App-button" onClick={()=>
+          {
+          if (count===4)
+            {window.open("https://www.youtube.com/watch?v=hHW1oY26kxQ")
+            {setcount(0)}}
+          else
+            {setcount(count + 1)}
+          }
+        }>
+          Click me five times for study music :)
+        </button>
+
+        <button onClick={()=>{window.open("http://www.cutestpaw.com/tag/dogs/</header>")}}>
+          Click me for cute dog photos 
+        </button>
+
+       
+
       </header>
     </div>
   );
